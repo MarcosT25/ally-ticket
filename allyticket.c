47065a7d4854b5f5a11566d913f3612f8c5029fc
Marcos Takahashi <m254311@dac.unicamp.br>
2021-11-24 14:54:54 -0300

    Feat: interface start

--- allyticket.c
+++ allyticket.c
@@ -3,6 +3,9 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define MOVIE_FILE "filmes.txt"
+#define SESSION_FILE "sessoes.txt"
+
 struct filme
 {
   char *nome;
@@ -14,64 +17,99 @@ typedef struct filme Filme;
 
 int tamanhoArquivo();
 void createFilme(Filme *nomeFilme);
-void read(char *arquivo);
+void readFilme();
 void updateFilme(int filmeIndex, int filmeCampo);
 void deleteFilme(int filmeIndex);
+void readSession();
 
 int main()
 {
-  printf("Escolha o que deseja fazer:\n1 - Adicionar um filme à lista;\n2 - Visualizar todos os filmes\n3 - Editar um filme\n4 - Excluir um filme\n0 - Sair\nEscolha: ");
-  int escolha;
-  scanf("%d", &escolha);
-  while (escolha != 0) {
-    if (escolha == 1) {
-      Filme filme;
-      char nome[52];
-      printf("Digite o nome do filme a ser inserido: ");
-      while ( getchar() != '\n' );
-      scanf("%[^\n]", nome);
-      filme.nome = nome;
-      char classificacao[32];
-      printf("Digite a classificação do filme: ");
-      while ( getchar() != '\n' );
-      scanf("%[^\n]", classificacao);
-      filme.classificacao = classificacao;
-      char categoria[52];
-      printf("Digite a categoria do filme: ");
-      while ( getchar() != '\n' );
-      scanf("%[^\n]", categoria);
-      filme.categoria = categoria;
-      Filme *filmePtr = &filme;
-      createFilme(filmePtr);
+  printf("Você é:\n1 - Cliente;\n2 - Funcionário\n0 - Sair\nEscolha: ");
+  int escolhaPerfil;
+  scanf("%d", &escolhaPerfil);
+  while (escolhaPerfil != 0) {
+    if (escolhaPerfil == 1) {
+      printf("O que deseja fazer?\n1 - Ver filmes disponíveis;\n2 - Ver sessões disponíveis;\n3 - Comprar um ingresso\n0 - Sair\nEscolha: ");
+      int escolhaAcaoCliente;
+      scanf("%d", &escolhaAcaoCliente);
+      while (escolhaAcaoCliente != 0) {
+        if (escolhaAcaoCliente == 1) {
+          readFilme();
+        }
+        else if (escolhaAcaoCliente == 2) {
+          readSession();
+        }
+        else if (escolhaAcaoCliente == 3) {
+          readSession();
+          printf("Qual sessão deseja comprar?\nDigite o número da sessão: ");
+        }
+        else {
+          printf("Nenhuma ação corresponde ao valor inserido, por favor selecione uma ação válida.\n");
+        }
+      }
     }
-    else if (escolha == 2) {
-      read("allyticket.txt");
-    }
-    else if (escolha == 3) {
-      printf("Os filmes disponíveis para edição e seus respectivos números são:\n");
-      read("allyticket.txt");
-      printf("Digite o número do filme que deseja editar: ");
-      int escolhaFilme;
-      scanf("%d", &escolhaFilme);
-      printf("Qual campo deseja modificar?\n1 - Nome\n2 - Classificação\n3 - Categoria\nEscolha: ");
-      int escolhaCampo;
-      scanf("%d", &escolhaCampo);
-      updateFilme(escolhaFilme, escolhaCampo);
-    }
-    else if (escolha == 4) {
-      printf("Os filmes disponíveis para exclusão e seus respectivos números são:\n");
-      read("allyticket.txt");
-      printf("Digite o número do filme que deseja excluir: ");
-      int escolhaFilme;
-      scanf("%d", &escolhaFilme);
-      deleteFilme(escolhaFilme);
+    else if (escolhaPerfil == 2) {
+      printf("Escolha o que deseja fazer:\n1 - Adicionar um filme à lista;\n2 - Visualizar todos os filmes\n3 - Editar um filme\n4 - Excluir um filme\n0 - Sair\nEscolha: ");
+      int escolha;
+      scanf("%d", &escolha);
+      while (escolha != 0) {
+        if (escolha == 1) {
+          Filme filme;
+          char nome[52];
+          printf("Digite o nome do filme a ser inserido: ");
+          while ( getchar() != '\n' );
+          scanf("%[^\n]", nome);
+          filme.nome = nome;
+          char classificacao[32];
+          printf("Digite a classificação do filme: ");
+          while ( getchar() != '\n' );
+          scanf("%[^\n]", classificacao);
+          filme.classificacao = classificacao;
+          char categoria[52];
+          printf("Digite a categoria do filme: ");
+          while ( getchar() != '\n' );
+          scanf("%[^\n]", categoria);
+          filme.categoria = categoria;
+          Filme *filmePtr = &filme;
+          createFilme(filmePtr);
+        }
+        else if (escolha == 2) {
+          readFilme();
+        }
+        else if (escolha == 3) {
+          printf("Os filmes disponíveis para edição e seus respectivos números são:\n");
+          readFilme();
+          printf("Digite o número do filme que deseja editar: ");
+          int escolhaFilme;
+          scanf("%d", &escolhaFilme);
+          printf("Qual campo deseja modificar?\n1 - Nome\n2 - Classificação\n3 - Categoria\nEscolha: ");
+          int escolhaCampo;
+          scanf("%d", &escolhaCampo);
+          updateFilme(escolhaFilme, escolhaCampo);
+        }
+        else if (escolha == 4) {
+          printf("Os filmes disponíveis para exclusão e seus respectivos números são:\n");
+          readFilme();
+          printf("Digite o número do filme que deseja excluir: ");
+          int escolhaFilme;
+          scanf("%d", &escolhaFilme);
+          deleteFilme(escolhaFilme);
+        }
+        else {
+          printf("Nenhuma ação corresponde ao valor inserido, por favor selecione uma ação válida.\n");
+        }
+        printf("Escolha o que deseja fazer:\n1 - Adicionar um filme à lista;\n2 - Visualizar todos os filmes\n3 - Editar um filme\n4 - Excluir um filme\n0 - Sair\nEscolha: ");
+        scanf("%d", &escolha);
+      }
     }
     else {
       printf("Nenhuma ação corresponde ao valor inserido, por favor selecione uma ação válida.\n");
+      printf("Você é:\n1 - Cliente;\n2 - Funcionário\n0 - Sair\nEscolha: ");
+      scanf("%d", &escolhaPerfil);
     }
-    printf("Escolha o que deseja fazer:\n1 - Adicionar um filme à lista;\n2 - Visualizar todos os filmes\n3 - Editar um filme\n4 - Excluir um filme\n0 - Sair\nEscolha: ");
-    scanf("%d", &escolha);
   }
+
+  
 }
 
 int tamanhoArquivo() {
@@ -96,10 +134,9 @@ void createFilme(Filme *nomeFilme)
   fprintf(file, "Nome: %s\nClassificacao: %s\nCategoria: %s", nomeFilme->nome, nomeFilme->classificacao, nomeFilme->categoria);
   fclose(file);
 }
-void read(char *arquivo)
-{
+void readFilme() {
   FILE *file;
-  file = fopen(arquivo, "r");
+  file = fopen("filmes.txt", "r");
 
   if (file == NULL){
     printf("Arquivo nao encontrado.\n");
@@ -220,4 +257,25 @@ void deleteFilme(int filmeIndex)
 
   remove("allyticket.txt");
   rename("replace.tmp", "allyticket.txt");
+}
+
+void readSession() {
+  FILE *file;
+  file = fopen(SESSION_FILE, "r");
+
+  char filme[52];
+  char sala[5];
+  char lugares[5];
+  int sessaoIndex = 0;
+
+  while(!feof(file)) {
+    fgets(filme, sizeof(filme), file);
+    fgets(sala, sizeof(sala), file);
+    fgets(lugares, sizeof(lugares), file);
+    sessaoIndex++;
+    printf("Sessão número %d:\n", sessaoIndex);
+    printf("%sSala número: %sAssentos disponíveis: %s", filme, sala, lugares);
+  }  
+  printf("\n");
+  fclose(file);
 }
